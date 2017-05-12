7c53ec2a1fc92e349b9ce93e5422266e5e68d0aa
Corwin <corstreet@gmail.com>
2017-05-11 20:43:10 -0700

    sets up fetch in App component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('Mounted!');
+    //set url for the fetch (netflix roulette)
+    let url = 'http://netflixroulette.net/api/api.php?director=Quentin%20Tarantino'; //tarantino movies currently on Netflix
+    //call our fetch function and supply this url
+    this.fetchAPI(url);
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Netflix fetcher!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {this.state.data.map((data) => 
+            <div key={data.show_id}>
+              <h1>{data.show_title}</h1>
+              <img src={data.poster} alt={data.summary} />
+              <div>Runtime: {data.runtime}</div>
+              <p>{data.summary}</p>
+            </div>
+        )}
       </div>
     );
+  }
+
+  fetchAPI(url) {
+    fetch(url).then((resp) => resp.json()).then((data) => {
+      console.log(data);
+      this.setState({
+        data: data
+      })
+    })
   }
 }
 
