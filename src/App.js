b722c0c4173ad51c7896e967262d7282d1759b4c
Corwin <corstreet@gmail.com>
2017-05-26 22:45:55 -0700

    minor copy edit on h2

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Netflix fetcher!</h2>
+          <h2>React Netflix Fetcher!</h2>
         </div>
         <p className="App-intro">
         </p>
